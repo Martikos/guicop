43d77792652be52a61d71fa48939f14bd49c7f33
Marc Adam <marc.m.adam@gmail.com>
2012-04-03 02:16:11 +0300

    implemented readspec function for officer, and constructor for specobject

--- src/guicop/officer.java
+++ src/guicop/officer.java
@@ -6,10 +6,50 @@
  */
 package guicop;
 
+import parsers.*;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
 import org.antlr.stringtemplate.*;
 
 public class officer {
+    private ArrayList<specobject> objects;
 
+    public officer() {
+        objects = new ArrayList();
+    }
+
+    public void readSpec (String filePath) throws Exception {
+        ANTLRReaderStream in = new ANTLRReaderStream(new FileReader(filePath));
+        specLexer lexer = new specLexer(in);
+	CommonTokenStream tokens = new CommonTokenStream(lexer);
+	specParser parser = new specParser(tokens);
+	specParser.specobjects_return result = parser.specobjects();
+	
+        Tree ast = (Tree)result.getTree();
+        // now each child of this tree is an object by it self,
+        // need to convert it to a spec object (with all it's fields) and place it in the list
+
+        // for each user defined object in file
+        for (int i= 0; i < ast.getChildCount(); i++) {
+            Tree objectTree = ast.getChild(i);
+
+            // get variables
+            Tree variablesTree = objectTree.getChild(0);
+
+            // get properties
+            Tree properties = objectTree.getChild(1);
+
+            // get constraints
+            Tree constraints = objectTree.getChild(2);
+
+            
+        }
+
+    }
 }
--- src/guicop/specobject.java
+++ src/guicop/specobject.java
@@ -6,6 +6,38 @@
  */
 package guicop;
 
-public class specobject {
+import geometric.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+import org.antlr.stringtemplate.*;
+
+public class specobject {
+    private HashMap variables;
+    private Tree properties;
+    private Tree constraints;
+    
+    private String id;
+
+
+    public specobject(Tree v, Tree p, Tree c) {
+        variables = new HashMap();
+        
+        // get variables
+        for (int j= 0; j < v.getChildCount(); j++) {
+            Tree sameVariableTree = v.getChild(j);
+            String variableType = sameVariableTree.getText();
+            for (int k= 0; k < sameVariableTree.getChildCount(); k++) {
+                String variableName = sameVariableTree.getChild(k).getText();
+                variables.put(variableName, variableType);
+            }
+        }
+        // get properties
+        properties = p;
+        // get constraints
+        constraints = c;
+    }
 }
